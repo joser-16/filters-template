fdbf1532e4e5f5f34ac246985bdb844bb35c920d
José <joserdp16@gmail.com>
2021-04-05 14:06:47 -0400

    Validaciones con validator.w3c.org

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const menuBtn = document.querySelector('.js-menu-btn')
 const menu = document.querySelector('.js-menu')
+const formPrice = document.querySelector('.js-form-price')
 
 menuBtn.addEventListener('click', function() {
   menu.classList.toggle('open')
   this.classList.toggle('open')
 })
+
+formPrice.addEventListener('submit', e => {
+  e.preventDefault()  
+})
+
